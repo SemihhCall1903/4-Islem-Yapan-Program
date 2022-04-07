6ea9beb749c8edb1db83c8e7210623477800d055
Semih Arda Çallı <77378627+SemihhCall1903@users.noreply.github.com>
2022-04-07 15:24:32 +0300

    Update SQL - Restoran Otomasyon.sql

--- SQL - Restoran Otomasyon.sql
+++ SQL - Restoran Otomasyon.sql
@@ -32,19 +32,21 @@ RestoranTedarikciTel varchar(11),
 RestoranTedarikciAdres varchar(100)
 )
 
+create table tbl_restoran_rezervasyon_masalar
+(
+RestoranRezervasyonMasaIdPk int primary key identity(15,15),
+RestoranRezervasyonMasaNo varchar(10),
+RestoranRezervasyonYaptýranMusteriIdPk int foreign key references tbl_restoran_musteriler(RestoranMusteriIdPk),
+RestoranRezervasyonYapanPersonelIdPk int foreign key references tbl_restoran_personeller(RestoranPersonelIdPk)
+)
+
 create table tbl_restoran_rezarvasyonlar
 (
 RestoranRezarvasyonIdPk int primary key identity(10,10),
 RestoranRezarvasyonNumara varchar(100),
-RestoranRezervasyonMasaNo varchar(100),
+RestoranRezervasyonMasaIdFk int foreign key references tbl_restoran_rezervasyon_masalar(RestoranRezervasyonIdPk),
 RestoranRezervasyonMenuIdFk int foreign key references tbl_restoran_menu_icerigi(RestoranMenuIcerigiIdPk),
 RestoranRezervasyonSaat varchar(100)
-)
-
-create table tbl_restoran_rezervasyon_masalar
-(
-RestoranRezervasyonMasaIdPk int primary key identity(15,15),
-RestoranRezervasyonMasaNo varchar(10)
 )
 
 create table tbl_restoran_stoklar
@@ -58,5 +60,4 @@ create table tbl_restoran_menuler
 (
 RestoranMenuIdPk int primary key identity(25,25),
 RestoranMenuIcerigiIdFk int foreign key references tbl_restoran_menu_icerigi(RestoranMenuIcerigiIdPk)
-
 )
